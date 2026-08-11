1b58cec53f59d8c0b4c6a0e42dff9cd20a4a277d
Hemadharshini003 <Hemadharshini003@gmail.com>
2026-08-11 21:12:36 +0530

    feat: implement authentication MVP

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from app.database import Base, engine
 from app.models.user import User
-
+from app.routers.auth import router as auth_router
+from fastapi.middleware.cors import CORSMiddleware
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="TrustHire AI")
+app.include_router(auth_router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
