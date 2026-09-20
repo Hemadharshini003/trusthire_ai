4881ad539a9740e64712ea557307b3dbdbfe3ad6
Hemadharshini003 <Hemadharshini003@gmail.com>
2026-09-20 17:01:35 +0530

    fix: configure production cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,5 @@
 import logging
-
+from app.config import FRONTEND_URL
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -75,3 +75,16 @@ def home():
     return {
         "message": "TrustHire AI Backend Running 🚀"
     }
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        FRONTEND_URL,
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://localhost:5174",
+        "http://127.0.0.1:5174",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
